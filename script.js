7ae553d004b7f2be655209b1a7ca15259d3e04d0
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-04-20 18:24:02 +0530

    fun game

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 100) + 1;
+let score = 100;
+let highScore = 0;
+const displayMessage = message =>
+  (document.querySelector('.message').textContent = message);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // when no input
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+  }
+  // when player won
+  else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+  // when palyer incorrect
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You Lost the Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
